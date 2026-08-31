bd2e3d19bf939af5a879e9c0e14a7efd33582e37
edwad <2576769085@qq.com>
2026-08-31 19:50:32 +0800

    chore: 移除开发调试脚本与临时文件(.workbuddy等)

--- .workbuddy/fix.py
+++
@@ -1,5 +0,0 @@
-﻿import sys
-content = open(r"D:\my-projects\agent-monitor\.workbuddy\patch_p7p8.py", encoding="utf-8").read()
-content = content.replace("+ NEW_msg +", "+ NEW_MSG +")
-open(r"D:\my-projects\agent-monitor\.workbuddy\patch_p7p8.py", "w", encoding="utf-8").write(content)
-print("Fixed")
--- .workbuddy/verify_js.py
+++
@@ -1,5 +0,0 @@
-lines = open(r'D:\my-projects\agent-monitor\viewer.py', 'rb').read().split(b'\n')
-keys = [b'sid=m', b'slice(2)', b'CURRENT_TRACE', b'focusSpan=function', b'set("trace"', b'indexOf("n_")']
-for i, l in enumerate(lines):
-    if any(k in l for k in keys):
-        print(i + 1, repr(l[:150]))
